34b27fe167e7a6487409b45c7faac076bf5bb209
Abdallah-Ali247 <aa2447454@gmail.com>
2025-04-20 21:30:03 +0200

    add restaurant owner function

--- backend/restaurants/serializers.py
+++ backend/restaurants/serializers.py
@@ -12,3 +12,16 @@ class RestaurantSerializer(serializers.ModelSerializer):
                   'logo', 'opening_time', 'closing_time', 'is_active', 'is_approved',
                   'rejection_reason', 'owner_id', 'owner_username', 'owner_details']
         read_only_fields = ['id']
+        
+    def get_owner_details(self, obj):
+        if not obj.owner:
+            return None
+            
+        return {
+            'id': obj.owner.id,
+            'username': obj.owner.username,
+            'email': obj.owner.email,
+            'first_name': obj.owner.first_name,
+            'last_name': obj.owner.last_name,
+            'user_type': obj.owner.user_type
+        }
