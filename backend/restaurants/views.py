256cd9cfb4709dba90a78006a91c988a8f944e47
Abdallah-Ali247 <aa2447454@gmail.com>
2025-04-20 21:34:07 +0200

    create is user function ??

--- backend/restaurants/views.py
+++ backend/restaurants/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status, filters
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from django.shortcuts import get_object_or_404
+from .models import Restaurant, Ingredient
+from .serializers import RestaurantSerializer, IngredientSerializer
 
-# Create your views here.
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Write permissions are only allowed to the owner or admin
+        return obj.owner == request.user or request.user.user_type == 'admin'
