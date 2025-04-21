4c73e1a5ba434aafdcf76189057a619a8d5e6c45
ahmed-hamediTi <ahmedhamed3375@gmail.com>
2025-04-21 21:32:44 +0200

    new commit

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -17,3 +17,17 @@ class RestaurantReview(models.Model):
     
     def __str__(self):
         return f"Review for {self.restaurant.name} by {self.user.username}"
+    
+class MealReview(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='meal_reviews')
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        unique_together = ('user', 'meal')
+    
+    def __str__(self):
+        return f"Review for {self.meal.name} by {self.user.username}"
