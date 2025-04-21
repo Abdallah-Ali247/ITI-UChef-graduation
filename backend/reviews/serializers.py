739dbae29ee9a65cedfb4a82ff35a38fea51fb4a
ahmed-hamediTi <ahmedhamed3375@gmail.com>
2025-04-21 22:18:07 +0200

    new commit

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -22,3 +22,14 @@ class MealReviewSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'user_username', 'meal', 'meal_name', 
                   'rating', 'comment', 'created_at', 'updated_at']
         read_only_fields = ['id', 'user', 'created_at', 'updated_at']  # Add user to read_only_fields
+
+class CustomMealReviewSerializer(serializers.ModelSerializer):
+    user_username = serializers.ReadOnlyField(source='user.username')
+    custom_meal_name = serializers.ReadOnlyField(source='custom_meal.name')
+    user = serializers.PrimaryKeyRelatedField(read_only=True)  # Make user field read-only
+    
+    class Meta:
+        model = CustomMealReview
+        fields = ['id', 'user', 'user_username', 'custom_meal', 'custom_meal_name', 
+                  'rating', 'comment', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'user', 'created_at', 'updated_at']  # Add user to read_only_fields
